3cac93bd54e36ea91c9c28af77b770d58faebdff
nida-auliyah <nidaauliyah@gmail.com>
2020-02-28 09:23:37 +0700

    mainActivity,matchActivity,resultActivty(ngepush langsung karena kemarin bisa ngepush diandroid tapi tidak kepush di github)

--- app/src/main/java/id/putraprima/skorbola/ResultActivity.java
+++ app/src/main/java/id/putraprima/skorbola/ResultActivity.java
@@ -3,12 +3,24 @@ package id.putraprima.skorbola;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ResultActivity extends AppCompatActivity {
 
+    private TextView hasilText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+        hasilText = findViewById(R.id.textView3);
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+
+            // TODO: display value here
+            Bundle extra = getIntent().getExtras();
+
+            hasilText.setText(extras.getString("hasil"));
+
+        }
     }
 }
